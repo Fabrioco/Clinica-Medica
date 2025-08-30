d8ca0a9f0ea7421d8c991c97ab42080cf757f0a6
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-08-29 22:58:00 -0300

    fix: corrigir a importação do Request

--- server/src/@types/express.d.ts
+++ server/src/@types/express.d.ts
@@ -1,5 +1,5 @@
 import { Role } from '@prisma/client';
-import 'express';
+import { Request } from 'express-serve-static-core';
 
 declare module 'express-serve-static-core' {
   interface Request {
