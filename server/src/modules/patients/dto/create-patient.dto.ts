c8d985920f181dad08ea2ce901406df7ce05fdc8
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-09-04 00:55:57 -0300

    feat: adicionei as validações do class-validator

--- server/src/modules/patients/dto/create-patient.dto.ts
+++ server/src/modules/patients/dto/create-patient.dto.ts
@@ -1,3 +1,4 @@
+import { IsDate, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePatientDto {
@@ -6,24 +7,28 @@ export class CreatePatientDto {
     description: 'CPF do paciente',
     uniqueItems: true,
   })
+  @IsString()
   cpf: string;
 
   @ApiProperty({
     example: '2023-05-01T12:00:00.000Z',
     description: 'Data de nascimento do paciente',
   })
+  @IsDate()
   birthDate: Date;
 
   @ApiProperty({
     example: '(11) 99999-9999',
     description: 'Telefone do paciente',
   })
+  @IsString()
   phone: string;
 
   @ApiProperty({
     example: 'Rua das Flores, 123',
     description: 'Endereço do paciente',
   })
+  @IsString()
   address: string;
 
   @ApiProperty({
@@ -31,5 +36,6 @@ export class CreatePatientDto {
     description: 'Plano de saude do paciente',
     required: false,
   })
+  @IsString()
   healthPlan: string;
 }
