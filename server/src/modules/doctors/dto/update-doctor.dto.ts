16e4962880221af77051a26ced5470a639b0864b
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-09-04 00:54:15 -0300

    feat: adicionei a tipagem de validação do class-validator

--- server/src/modules/doctors/dto/update-doctor.dto.ts
+++ server/src/modules/doctors/dto/update-doctor.dto.ts
@@ -1,21 +1,29 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { CreateDoctorDto } from './create-doctor.dto';
 
-export class UpdateDoctorDto {
+export class UpdateDoctorDto extends PartialType(CreateDoctorDto) {
   @ApiProperty({
     example: '123456',
     description: 'CRM do médico',
   })
+  @IsString()
+  @IsOptional()
   crm?: string;
 
   @ApiProperty({
     example: 'Cardiologist',
     description: 'Especialidade do médico',
   })
+  @IsString()
+  @IsOptional()
   specialty?: string;
 
   @ApiProperty({
     example: '(11) 99999-9999',
     description: 'Telefone do médico',
   })
+  @IsString()
+  @IsOptional()
   phone?: string;
 }
