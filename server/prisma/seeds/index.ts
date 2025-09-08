fe42e1bdc66abff2c7075efab0475491a98e06a7
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-09-07 23:34:53 -0300

    feat: adicionei o seed de patient

--- server/prisma/seeds/index.ts
+++ server/prisma/seeds/index.ts
@@ -1,7 +1,9 @@
+import { patientSeeds } from './modules/patient.seed';
 import { usersSeeds } from './modules/users.seed';
 
 async function main() {
   await usersSeeds();
+  await patientSeeds();
 }
 
 main();
