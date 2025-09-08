66c25cad313f28329f0c82582cf9b2b1e9442251
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-09-07 23:03:11 -0300

    feat: adicionei as rotas de forgot password e reset password separadas das rotas auth

---
+++ server/src/modules/no-auth/dto/reset-password.dto.ts
@@ -0,0 +1,31 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail } from 'class-validator';
+
+export class ResetPasswordDto {
+  @ApiProperty({
+    example: 'user@example.com',
+    description: 'The email of the user',
+    required: true,
+  })
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({
+    example: 312443,
+    description: 'Code for reset password',
+    required: true,
+  })
+  code: number;
+  @ApiProperty({
+    example: 'User@123',
+    description: 'The password of the user',
+    required: true,
+  })
+  password: string;
+  @ApiProperty({
+    example: 'User@123',
+    description: 'The password of the user',
+    required: true,
+  })
+  confirmPassword: string;
+}
